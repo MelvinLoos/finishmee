a5de4bd14f4ca01b7f6f02674c92582509149ede
Melvin <melvin@melvinloos.nl>
2020-05-02 14:09:08 +0200

    Getest met styling

--- index.php
+++ index.php
@@ -13,11 +13,18 @@
 
     var_dump($_SESSION);
 ?>
+
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="mystyle.css" >
     </head>
     <body>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
         <h1>Finish Mee</h1>
 
         <?php if (isset($_SESSION['name'])): ?>
